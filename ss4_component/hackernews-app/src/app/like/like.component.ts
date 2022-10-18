336d6297309c1a7e2cb22a83cfe062f8a00b673d
thuytiendang <thuytiendang2003@gmail.com>
2022-10-18 12:09:53 +0700

    m

---
+++ ss4_component/hackernews-app/src/app/like/like.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+  like = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  likeThis() {
+    this.like++;
+  }
+}
